ed84facad6348379be5adfc0d9516bf4a85f43b2
Hassan Mohammadian <hasssanitman@gmail.com>
2023-06-15 13:16:15 +0330

    f2

--- main.go
+++ main.go
@@ -18,7 +18,9 @@ func main() {
 		}
 	}
 
-	f1([]int{1, 3, 5, 4, 2, 6, 7, 8})
+	f1([]int{1, 3, 5, 4, 2, 6, 7, 8, 9})
+	ar1, ar2 := f2([]int{1, 3, 1, 4, 3, 6, 8, 8, 9})
+	fmt.Println(ar1, ar2)
 }
 
 // [1,3,5,4,2]
@@ -27,6 +29,29 @@ func f1(arr []int) {
 	for _, v := range arr {
 		if v == len {
 			fmt.Println(v + 1)
+			break
 		}
 	}
+}
+
+func f2(arr []int) ([]int, []int) {
+	maped := make(map[int]int)
+	for _, v := range arr {
+		if _, ok := maped[v]; ok {
+			maped[v] += 1
+		} else {
+			maped[v] = 1
+		}
+	}
+	unique := make([]int, 0)
+	noUnique := make([]int, 0)
+
+	for k, v := range maped {
+		if v > 1 {
+			noUnique = append(noUnique, k)
+		} else {
+			unique = append(unique, k)
+		}
+	}
+	return unique, noUnique
 }
